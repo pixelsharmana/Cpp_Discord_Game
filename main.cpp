4bcc1fb1c20bf9f257e0546dabccc3ff89f35a37
Kevin Tindall <kevinkjt2000@users.noreply.github.com>
2017-09-17 16:41:06 -0500

    use the prompt function and use message parameter

--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 
 template<typename T>
 T prompt(std::string message) {
+    std::cout << message;
     T value;
+    std::cin >> value;
     return value;
 }
 int main() {
     std::string name;
-    std::cout << "Enter player name: " << std::flush;
-    std::cin >> name;
+    name = prompt<string>("Enter player name: ");
     std::cout << "Are you sure " << name << " is your name?\n";
 
     return 0;
